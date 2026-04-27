e5c39d7e94267192c4f4a9b35407ed944c33a3d1
Bennerth Gyamfi <gbennerth@gmail.com>
2026-04-27 14:15:08 +0000

    all done

--- lib/pages/real_signup_page.dart
+++ lib/pages/real_signup_page.dart
@@ -6,7 +6,6 @@ import 'package:get/get.dart';
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
-
   @override
   State<SignUpPage> createState() => _SignUpPageState();
 }
